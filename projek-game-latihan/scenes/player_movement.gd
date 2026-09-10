fc0a737f1b7510523c7c016f311c6e56097ba502
Bara Maamea <abrahamrainhard123@gmail.com>
2026-09-10 22:45:07 +0700

    Jumpscare system fix (balik ke main menu skg)

--- projek-game-latihan/scenes/player_movement.gd
+++ projek-game-latihan/scenes/player_movement.gd
@@ -98,7 +98,7 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 			game_over()
 
 func game_over():
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _physics_process(_delta: float) -> void:
 	get_input()
