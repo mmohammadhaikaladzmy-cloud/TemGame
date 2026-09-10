b0e3ef218f4791ca469c879f521a90063e4bfaca
Pheliex <roiyanfelix7@gmail.com>
2026-09-10 14:02:18 +0700

    map hutan angker ngeri

--- projek-game-latihan/scenes/player_movement.gd
+++ projek-game-latihan/scenes/player_movement.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @onready var detection_shape = $detect_area/CollisionShape2D
 @onready var flashlight_on = false
 @onready var flashlight_light = $PointLight2D
+@onready var flashlight_light2 = $PointLight2D
 
 func _ready() -> void:
 	add_to_group("player")
@@ -22,10 +23,12 @@ func get_input():
 		if flashlight_on:
 			detection_shape.global_scale = Vector2(21,21)
 			flashlight_light.visible = true
+			flashlight_light2.visible = true
 			print("flashlight on")
 		else:
 			detection_shape.global_scale = Vector2(7.4,7.4)
 			flashlight_light.visible = false
+			flashlight_light2.visible = false
 			print("flashlight off")
 
 func _on_detection_area_entered(area: Area2D) -> void:
